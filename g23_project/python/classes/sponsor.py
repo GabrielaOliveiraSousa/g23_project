07cea04dd17b9697265418619d40f575cbef698c
leonorgcarvalho <up202508328@up.pt>
2026-04-26 17:28:06 +0100

    Update sponsor.py

--- g23_project/python/classes/sponsor.py
+++ g23_project/python/classes/sponsor.py
@@ -7,19 +7,19 @@ class Sponsor(Gclass):
     pos = 0
     sortkey = ''
     att = ['_sponsor_id','_extra_info','_podcast_id']
-    header = 'PodcastSponsor'
+    header = 'Sponsor'
     des = ['Sponsor_id','Extra_info','Podcast_id']
     def __init__(self, id, extra_info, podcast_id):
         super().__init__()
         podcast_id =int(podcast_id)
         if podcast_id in Podcast.lst:
-            id = PodcastSponsor.get_id(id)
+            id =Sponsor.get_id(id)
             self._id = id
             self._extra_info=extra_info
             self._podcast_id = podcast_id
 
-            PodcastSponsor.obj[id] = self
-            PodcastSponsor.lst.append(id)
+            Sponsor.obj[id] = self
+            Sponsor.lst.append(id)
         else:
             print('Sponsor', podcast_id, ' not found')
     @property
