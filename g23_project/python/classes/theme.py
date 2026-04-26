cfb962c47ecbb9302bd9e58d3959dcd69db0c086
alexandraleao <up202505502@up.pt>
2026-04-26 16:44:36 +0100

    Update theme.py

--- g23_project/python/classes/theme.py
+++ g23_project/python/classes/theme.py
@@ -4,7 +4,6 @@ Created on Mon Apr 20 18:11:52 2026
 
 @author: axlea
 """
-
 from classes.podcast import Podcast
 from classes.gclass import Gclass
 
@@ -13,12 +12,17 @@ class PodcastTheme(Gclass):
     lst = list()
     pos = 0
     sortkey = ''
-    att = ['_theme_id','_subject','_podcast_id']
+
+
+    att = ['_id', '_subject', '_podcast_id']
 
     header = 'Theme'
-    des = ['Theme_Id','Subject','Podcast_id']
+    des = ['Theme_Id', 'Subject', 'Podcast_id']
 
-    def __init__(self, theme_id, subject, podcast_id):
+
+    table = 'Theme'
+
+    def __init__(self, id, subject, podcast_id):
         super().__init__()
 
         try:
@@ -27,33 +31,46 @@ class PodcastTheme(Gclass):
             print('Invalid podcast_id')
             return
 
-        if podcast_id in Podcast.lst:
-            theme_id = PodcastTheme.get_id(theme_id)
-            self._theme_id = theme_id
-            self._subject = subject
-            self._podcast_id = podcast_id
+        if podcast_id not in Podcast.lst:
+            print('Podcast', podcast_id, 'not found')
+            return
 
-            PodcastTheme.obj[theme_id] = self
-            PodcastTheme.lst.append(theme_id)
-        else:
-            print('Podcast ', podcast_id, ' not found')
 
+        id = PodcastTheme.get_id(id)
+
+
+        self._id = id
+        self._subject = subject
+        self._podcast_id = podcast_id
+
+        PodcastTheme.obj[id] = self
+        PodcastTheme.lst.append(id)
+
+  
+    @property
+    def id(self):
+        return self._id
+
+    # opcional: manter compatibilidade com o nome antigo
     @property
     def theme_id(self):
-        return self._theme_id
+        return self._id
+
     @property
     def subject(self):
         return self._subject
+
     @subject.setter
     def subject(self, subject):
         self._subject = subject
+
     @property
     def podcast_id(self):
         return self._podcast_id
+
     @podcast_id.setter
     def podcast_id(self, podcast_id):
         if podcast_id in Podcast.lst:
             self._podcast_id = podcast_id
         else:
-            print('Podcast ', podcast_id, ' not found')    
-            
+            print('Podcast', podcast_id, 'not found')
