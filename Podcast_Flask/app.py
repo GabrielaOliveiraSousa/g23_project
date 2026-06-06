2a2bae2b505d77e1b37aa3a991b99c642a2b3405
BarbaraBraganca <up202510084@up.pt>
2026-06-06 16:01:53 +0100

    Update app.py

--- Podcast_Flask/app.py
+++ Podcast_Flask/app.py
@@ -1,28 +1,36 @@
+
 from flask import Flask, render_template, request, session
-from datafile import filename        
-import subs.index_subs as indexSubs
+from classes.guest import Guest
+from datafile import filename
+from classes.participation import Participation
+from classes.podcast import Podcast
+from classes.sponsor import Sponsor
+from classes.theme import Theme
 from classes.userlogin import Userlogin
+from subs.apps_podcast import apps_podcast
+from subs.apps_gform import apps_gform 
+from subs.apps_subform import apps_subform 
+from subs.apps_userlogin import apps_userlogin
 
 app = Flask(__name__)
 
-Userlogin.read(filename + 'DadaBase_Podcast.db')
-prev_option=''
-app.secret_key = 'BAD_SECRET_KEY'  
-
-path = filename + 'DadaBase_Podcast.db'
-
-@app.route("/", methods=["POST", "GET"])
+Person.read(filename + 'Person.db')
+Customer.read(filename + 'business.db')
+Product.read(filename + 'business.db')
+CustomerOrder.read(filename + 'business.db')
+OrderProduct.read(filename + 'business.db')
+Userlogin.read(filename + 'business.db')
+app.secret_key = 'BAD_SECRET_KEY'
+@app.route("/")
 def index():
-    return indexSubs.index(path)
+    return render_template("index.html", ulogin=session.get("user"))
 @app.route("/login")
 def login():
-    return render_template("login.html", id=0, user="", password="", ulogin=session.get("user"), resul="")
-
+    return render_template("login.html", user= "", password="", ulogin=session.get("user"),resul = "")
 @app.route("/logoff")
 def logoff():
-    session.pop("user", None)
+    session.pop("user",None)
     return render_template("index.html", ulogin=session.get("user"))
-
 @app.route("/chklogin", methods=["post","get"])
 def chklogin():
     user = request.form["user"]
@@ -31,7 +39,18 @@ def chklogin():
     if resul == "Valid":
         session["user"] = user
         return render_template("index.html", ulogin=session.get("user"))
-    return render_template("login.html", user=user, password=password, ulogin=session.get("user"), resul=resul)
-
+    return render_template("login.html", user=user, password = password, ulogin=session.get("user"),resul = resul)
+@app.route("/Person", methods=["post","get"])
+def person():
+    return apps_person()
+@app.route("/gform/<cname>", methods=["post","get"])
+def gform(cname):
+    return apps_gform(cname)
+@app.route("/subform/<cname>", methods=["post","get"])
+def subform(cname):
+    return apps_subform(cname)
+@app.route("/Userlogin", methods=["post","get"])
+def userlogin():
+    return apps_userlogin()
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    app.run()
