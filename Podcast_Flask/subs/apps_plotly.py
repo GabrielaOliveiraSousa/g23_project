ea1ae3755f4ecd8bff0907363bc9580658f12c1a
leonorgcarvalho <up202508328@up.pt>
2026-06-06 17:50:58 +0100

    Update apps_plotly.py

--- Podcast_Flask/subs/apps_plotly.py
+++ Podcast_Flask/subs/apps_plotly.py
@@ -1,50 +1,31 @@
 from flask import render_template, session
+from classes.podcast import Podcast
+from classes.sponsor import Sponsor
+from classes.theme import Theme
+from classes.participation import Participation
+from classes.guest import Guest
+from datafile import filename
+
 import pandas as pd
+from sqlalchemy import create_engine
 import plotly.express as px
 
-@app.route('/plotly')
 def apps_plotly():
+  df = pd.read_csv('g23_Podcasts_Guests.csv', sep=';', header=1)
+  df.columns = df.columns.str.strip()
+  
+  df['date'] = pd.to_datetime(df['date'], dayfirst=True, errors='coerce')
+  df['creation_date'] = pd.to_datetime(df['creation_date'], dayfirst=True, errors='coerce')
 
-    df = pd.read_csv('g23_Podcasts_Guests.csv', sep=';', header=1)
-    df.columns = df.columns.str.strip()
+  top_convidados = df['name'].value_counts().head(10).reset_index()
+  top_convidados.columns = ['Convidado', 'Participações']
+    
+  fig_participacoes = px.bar(top_convidados,x='Participações', y='Convidado', orientation='h',title='Top 10 Convidados com Mais Participações no Podcast',labels={'Participações': 'Número de Episódios', 'Convidado': 'Nome do Convidado'},color='Participações',color_continuous_scale='Tealgrn')
+  
+  fig_participacoes.update_layout(yaxis={'categoryorder': 'total ascending'}, showlegend=False,template='plotly_white')
+  fig_participacoes.update_traces(texttemplate='%{x}', textposition='outside')
 
-    print(df.columns.tolist()) 
+  plot_div = fig_participacoes.to_html(full_html=False, div_id='my-plot')
 
-    df['date'] = pd.to_datetime(df['date'], dayfirst=True, errors='coerce')
-    df['creation_date'] = pd.to_datetime(df['creation_date'], dayfirst=True, errors='coerce')
 
-    top_convidados = df['name'].value_counts().head(10).reset_index()
-    top_convidados.columns = ['Convidado', 'Participações']
-
-    fig = px.bar(
-        top_convidados,
-        x='Participações',
-        y='Convidado',
-        orientation='h',
-        title='Top 10 Convidados com Mais Participações no Podcast',
-        labels={
-            'Participações': 'Número de Episódios',
-            'Convidado': 'Nome do Convidado'
-        },
-        color='Participações',
-        color_continuous_scale='Tealgrn'
-    )
-
-    fig.update_layout(
-        yaxis={'categoryorder': 'total ascending'},
-        template='plotly_white',
-        showlegend=False
-    )
-
-    fig.update_traces(
-        texttemplate='%{x}',
-        textposition='outside'
-    )
-
-    plot_div = fig.to_html(full_html=False)
-
-    return render_template(
-        'plotly.html',
-        plot_div=plot_div,
-        ulogin=session.get("user")
-    )
+  return render_template("plotly.html", plot_div=plot_div, ulogin=session.get("user"))
